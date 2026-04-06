ed51864eb3db05d4bcd4d28c684d06b1b07b705f
Mohammed El-Mustafa Ahmed <m.m.elhag.elamin@gmail.com>
2026-04-06 20:41:25 +0100

    Update .config/sway/scripts/check_media.sh

--- dot_config/sway/scripts/executable_check_media.sh
+++ dot_config/sway/scripts/executable_check_media.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 reset_timer() {
-    wlrctl pointer move 0 0
+    wlrctl pointer click 11
 }
 
 # Something is playing
 if playerctl status 2>/dev/null | grep -q "Playing"; then
     reset_timer
-    exit 1 # Media is playing, do NOT dim
+    exit 1
 fi
 
 # Some audio is outputing
