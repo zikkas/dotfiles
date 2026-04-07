2ec9c0d55d351ea0744c4701b953f608000228c0
Mohammed El-Mustafa Ahmed <m.m.elhag.elamin@gmail.com>
2026-04-07 18:57:05 +0100

    Update .config/sway/scripts/check_media.sh

--- dot_config/sway/scripts/executable_check_media.sh
+++ dot_config/sway/scripts/executable_check_media.sh
@@ -5,15 +5,10 @@ reset_timer() {
 }
 
 # Something is playing
-if playerctl status 2>/dev/null | grep -q "Playing"; then
+if playerctl status 2>/dev/null | grep -q "Playing" || \
+   wpctl status | grep -A 10 "Streams:" | grep -q "running"; then
     reset_timer
     exit 1 # Media is playing, do NOT dim
-fi
-
-# Some audio is outputing
-if wpctl status | grep -A 10 "Streams:" | grep -q "running"; then
-    reset_timer
-    exit 1
 fi
 
 exit 0
