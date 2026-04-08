f5c6393a10a9433e8ea09a57edfc66cd5f9b7dc8
Mohammed El-Mustafa Ahmed <m.m.elhag.elamin@gmail.com>
2026-04-08 19:54:22 +0100

    Update .config/sway/scripts/lock.sh

--- dot_config/sway/scripts/executable_lock.sh
+++ dot_config/sway/scripts/executable_lock.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 
-sleep 5
-
-swaylock \
-  --screenshots \
-  --clock \
-  --indicator \
-  --indicator-radius 100 \
-  --indicator-thickness 7 \
-  --effect-blur 7x5 \
-  --effect-vignette 0.5:0.5 \
-  --ring-color 458588 \
-  --key-hl-color ebdbb2 \
-  --text-color ebdbb2 \
-  --line-color 00000000 \
-  --inside-color 00000088 \
-  --separator-color 00000000 \
-  --fade-in 0.2
+hyprlock
